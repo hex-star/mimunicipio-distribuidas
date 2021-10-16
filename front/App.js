8dfefadedcdbfcd6d2a4064da40157c2f2603efb
Motta7000 <agus.motta@hotmail.com>
2021-10-16 00:03:17 -0300

    Arme un prototipo de la home screen y la logica para ir a la pantalla de registrar

--- front/App.js
+++ front/App.js
@@ -3,12 +3,33 @@ import { Provider as PaperProvider } from 'react-native-paper';
 import theme from './customProperties/Themes';
 import TopBar from './components/TopBar';
 import MainScreen from './components/MainScreen';
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+import HomeScreen from './screens/HomeScreen';
+import Registrar from './screens/Registrar';
+import {Text,TouchableOpacity} from 'react-native'
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
     <PaperProvider theme={theme}>
       <TopBar />
-      <MainScreen />
+
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name ="HomeScreen" component={HomeScreen} options={({navigation}) =>({
+          headerRight:() => (<TouchableOpacity onPress={() =>navigation.navigate('Registrar')}>
+          <Text style={{color:"black",marginRight:20,fontSize:15}}>New</Text>
+          </TouchableOpacity>)
+          })}>
+
+          </Stack.Screen>
+          <Stack.Screen name ="Registrar" component={Registrar}>
+
+          </Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
---
+++ front/screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+import MainScreen from '../components/MainScreen';
+
+
+const HomeScreen = ({navigation}) => (<MainScreen navigation = {navigation}/>
+
+);
+
+export default HomeScreen;
