cf62fcef7fd8611fd31861149617f7d6ffd1dbdc
Nicolás Estrella <estrella.nico@gmail.com>
2021-10-18 18:36:51 -0300

    Eslint comentado

--- front/components/MainScreen.js
+++ front/components/MainScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import {
+  View, Text, StyleSheet, TouchableOpacity, Image,
+} from 'react-native';
 import { withTheme } from 'react-native-paper';
 
 function MainScreen(props) {
@@ -7,19 +9,29 @@ function MainScreen(props) {
   const { colors, fonts } = theme;
   return (
     <View style={style.itemContainer}>
-      <Image source={require("./../assets/favicon.png")} />
+      <Image source={require('../assets/favicon.png')} />
       <Text style={fonts.title1}>MiVecindario</Text>
       <Text style={{ color: colors.primary }}>Texto con color</Text>
 
       <TouchableOpacity onPress={() => props.navigation.navigate('Registrar')}>
-        <Text style={{ color: "white", fontSize: 15, borderColor: 'white', borderWidth: 2, borderRadius: 10, padding: 5 }}>Continuar como invitado</Text>
+        <Text style={{
+          color: 'white', fontSize: 15, borderColor: 'white', borderWidth: 2, borderRadius: 10, padding: 5,
+        }}
+        >
+          Continuar como invitado
+        </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={{marginTop:5,backgroundColor:'white'}} onPress={() => props.navigation.navigate('Registrar')}>
-        <Text style={{ color:colors.primary, fontSize: 15, borderColor: 'white', borderWidth: 2, borderRadius: 10, padding: 5 }}>Iniciar sesion</Text>
+      <TouchableOpacity style={{ marginTop: 5, backgroundColor: 'white' }} onPress={() => props.navigation.navigate('Registrar')}>
+        <Text style={{
+          color: colors.primary, fontSize: 15, borderColor: 'white', borderWidth: 2, borderRadius: 10, padding: 5,
+        }}
+        >
+          Iniciar sesion
+        </Text>
       </TouchableOpacity>
-      <Text style={{marginTop:20}}>¿No estas registrado? Registrate aca</Text>
-      <Text style={{marginTop:20}}>Una aplicacion del municipio de posadas</Text>
+      <Text style={{ marginTop: 20 }}>¿No estas registrado? Registrate aca</Text>
+      <Text style={{ marginTop: 20 }}>Una aplicacion del municipio de posadas</Text>
     </View>
   );
 }
@@ -32,31 +44,30 @@ const style = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'column',
     margin: 10,
-    backgroundColor: '#4169E1'
+    backgroundColor: '#4169E1',
   },
   container: {
-    flex: .5,
+    flex: 0.5,
     flexDirection: 'row',
-    justifyContent: 'flex-start', //replace with flex-end or center
+    justifyContent: 'flex-start', // replace with flex-end or center
     borderBottomWidth: 1,
-    borderBottomColor: '#000'
+    borderBottomColor: '#000',
   },
   container2: {
-    flex: .5,
+    flex: 0.5,
     flexDirection: 'row',
-    alignItems: 'flex-start' //replace with flex-end or center
+    alignItems: 'flex-start', // replace with flex-end or center
   },
   itemTitle: {
     color: '#ffffff',
     paddingLeft: 10,
-    paddingTop: 10
+    paddingTop: 10,
   },
   itemDescription: {
     color: '#ffffff',
     paddingLeft: 10,
-    paddingBottom: 10
-  }
-})
-
+    paddingBottom: 10,
+  },
+});
 
 export default withTheme(MainScreen);
