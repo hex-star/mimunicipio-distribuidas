596f871d34f8b4743483635f98042dbf5d8111c9
Nestrella <56445739+hex-star@users.noreply.github.com>
2021-10-27 18:44:17 -0300

    agregué texto

--- front/screens/Perfil.js
+++ front/screens/Perfil.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import {
+    View, Text, Image, TextInput,
+} from 'react-native';
 import style from '../customProperties/Styles';
 import MiVecindario from '../components/MiVecindario';
 import logo from '../assets/avatar.png';
@@ -10,12 +12,20 @@ function Perfil() {
             <MiVecindario />
             <View style={style.formsContainer}>
                 <Text style={style.sectionTitle}>Mi perfil</Text>
-                <Image
-                    style={{
-                        width: 50, height: 50, resizeMode: 'stretch', justifyContent: 'center',
-                    }}
-                    source={logo}
-                />
+                <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+                    <Image
+                        style={{
+                            width: 80, height: 80, resizeMode: 'stretch', justifyContent: 'center',
+                        }}
+                        source={logo}
+                    />
+                </View>
+                <Text style={style.formTooltip}>Nombre</Text>
+                <Text style={{ fontSize: 12, color: 'black', textDecorationLine: 'underline' }}> Nicolás </Text>
+                <TextInput placeholder="Estrella" />
+                <Text style={style.formTooltip}>Apellido</Text>
+                <Text style={style.formTooltip}>Documento</Text>
+                <Text style={style.formTooltip}>Dirección</Text>
             </View>
         </>
     );
