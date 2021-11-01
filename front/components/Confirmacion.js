ebf3de3d146b0169c004352b993f1db8bfe749c3
Nestrella <56445739+hex-star@users.noreply.github.com>
2021-11-01 18:59:45 -0300

    estilo en pantalla confirmacion

--- front/components/Confirmacion.js
+++ front/components/Confirmacion.js
@@ -3,8 +3,10 @@ import {
     View,
     Text,
     TouchableOpacity,
+    Image,
 } from 'react-native';
 import style from '../customProperties/Styles';
+import logo from '../assets/ok_tick_icon.png';
 
 function MainScreen(props) {
     const { navigation, route } = props;
@@ -14,7 +16,20 @@ function MainScreen(props) {
     return (
         <>
             <View style={style.homeContainer}>
-                <Text style={style.whiteSubtitle1}>{`Se ha generado tu ${tipo} exitosamente`}</Text>
+                <View style={{ justifyContent: 'center' }}>
+                    <Image source={logo} resizeMode="stretch" style={{ width: 200, height: 200 }} />
+                </View>
+                <Text style={{
+                    color: 'white',
+                    alignSelf: 'center',
+                    fontSize: 22,
+                    marginVertical: 10,
+                    marginLeft: 10,
+                    textAlign: 'center',
+                }}
+                >
+                    {`Se ha generado tu ${tipo} exitosamente`}
+                </Text>
                 <Text style={style.whiteSubtitle2}>{`Id ${tipo}: ${id}`}</Text>
                 <TouchableOpacity
                     style={style.homeButtonPrimary}
